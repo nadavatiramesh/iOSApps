a178b8aca50bb25f60a65ef34acb309a7af0e0ea
nadavatiramesh <nadavati.r76@gmail.com>
2018-04-10 16:32:36 -0400

    simple Hello World App..

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by ramesh nadavati on 4/6/18.
+//  Copyright © 2018 ramesh nadavati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
